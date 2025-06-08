a980734d356254234c4de26482ec8fb1fa1dbc70
jagobainda <jagobainda@gmail.com>
2025-06-08 04:36:13 +0200

    Agregar archivos de proyecto.

---
+++ SkinHolderDesktop/Core/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace SkinHolderDesktop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
