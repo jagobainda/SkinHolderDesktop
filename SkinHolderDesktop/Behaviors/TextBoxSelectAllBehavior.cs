9bdb53f7f2e7de6621f7ff78a436ffe0ae68f6ed
jagobainda <jagobainda@gmail.com>
2025-12-03 02:58:05 +0000

    UserItem search improvement

---
+++ SkinHolderDesktop/Behaviors/TextBoxSelectAllBehavior.cs
@@ -0,0 +1,60 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SkinHolderDesktop.Behaviors
+{
+    public static class TextBoxSelectAllBehavior
+    {
+        public static readonly DependencyProperty SelectAllOnFocusProperty =
+            DependencyProperty.RegisterAttached(
+                "SelectAllOnFocus",
+                typeof(bool),
+                typeof(TextBoxSelectAllBehavior),
+                new PropertyMetadata(false, OnSelectAllOnFocusChanged));
+
+        public static bool GetSelectAllOnFocus(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(SelectAllOnFocusProperty);
+        }
+
+        public static void SetSelectAllOnFocus(DependencyObject obj, bool value)
+        {
+            obj.SetValue(SelectAllOnFocusProperty, value);
+        }
+
+        private static void OnSelectAllOnFocusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBox textBox)
+            {
+                if ((bool)e.NewValue)
+                {
+                    textBox.GotFocus += TextBox_GotFocus;
+                    textBox.PreviewMouseLeftButtonDown += TextBox_PreviewMouseLeftButtonDown;
+                }
+                else
+                {
+                    textBox.GotFocus -= TextBox_GotFocus;
+                    textBox.PreviewMouseLeftButtonDown -= TextBox_PreviewMouseLeftButtonDown;
+                }
+            }
+        }
+
+        private static void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                textBox.SelectAll();
+            }
+        }
+
+        private static void TextBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is TextBox textBox && !textBox.IsKeyboardFocusWithin)
+            {
+                textBox.Focus();
+                e.Handled = true;
+            }
+        }
+    }
+}
