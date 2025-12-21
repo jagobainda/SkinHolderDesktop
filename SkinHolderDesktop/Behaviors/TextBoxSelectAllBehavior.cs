f9de13b290421688a7388649ebc58ec9e87f6349
jagobainda <jagobainda@gmail.com>
2025-12-21 19:32:54 +0100

    Enhance SelectAllOnFocus behavior; update version & branding
    Refactor TextBoxSelectAllBehavior to support both TextBox and PasswordBox, enabling select-all-on-focus for both controls. Apply this behavior to login and user item input fields for improved UX. Update project version to 0.8.0 and rebrand from "Skinholder Desktop" (CS:GO) to "Skinholder" (Counter-Strike). Add new files to .gitignore.

--- SkinHolderDesktop/Behaviors/TextBoxSelectAllBehavior.cs
+++ SkinHolderDesktop/Behaviors/TextBoxSelectAllBehavior.cs
@@ -2,59 +2,85 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace SkinHolderDesktop.Behaviors
+namespace SkinHolderDesktop.Behaviors;
+
+public static class TextBoxSelectAllBehavior
 {
-    public static class TextBoxSelectAllBehavior
+    public static readonly DependencyProperty SelectAllOnFocusProperty =
+        DependencyProperty.RegisterAttached(
+            "SelectAllOnFocus",
+            typeof(bool),
+            typeof(TextBoxSelectAllBehavior),
+            new PropertyMetadata(false, OnSelectAllOnFocusChanged));
+
+    public static bool GetSelectAllOnFocus(DependencyObject obj)
     {
-        public static readonly DependencyProperty SelectAllOnFocusProperty =
-            DependencyProperty.RegisterAttached(
-                "SelectAllOnFocus",
-                typeof(bool),
-                typeof(TextBoxSelectAllBehavior),
-                new PropertyMetadata(false, OnSelectAllOnFocusChanged));
+        return (bool)obj.GetValue(SelectAllOnFocusProperty);
+    }
 
-        public static bool GetSelectAllOnFocus(DependencyObject obj)
-        {
-            return (bool)obj.GetValue(SelectAllOnFocusProperty);
-        }
+    public static void SetSelectAllOnFocus(DependencyObject obj, bool value)
+    {
+        obj.SetValue(SelectAllOnFocusProperty, value);
+    }
 
-        public static void SetSelectAllOnFocus(DependencyObject obj, bool value)
-        {
-            obj.SetValue(SelectAllOnFocusProperty, value);
-        }
+    private static void OnSelectAllOnFocusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        bool newValue = (bool)e.NewValue;
 
-        private static void OnSelectAllOnFocusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        if (d is TextBox textBox)
         {
-            if (d is TextBox textBox)
+            if (newValue)
             {
-                if ((bool)e.NewValue)
-                {
-                    textBox.GotFocus += TextBox_GotFocus;
-                    textBox.PreviewMouseLeftButtonDown += TextBox_PreviewMouseLeftButtonDown;
-                }
-                else
-                {
-                    textBox.GotFocus -= TextBox_GotFocus;
-                    textBox.PreviewMouseLeftButtonDown -= TextBox_PreviewMouseLeftButtonDown;
-                }
+                textBox.GotFocus += TextBox_GotFocus;
+                textBox.PreviewMouseLeftButtonDown += TextBox_PreviewMouseLeftButtonDown;
+            }
+            else
+            {
+                textBox.GotFocus -= TextBox_GotFocus;
+                textBox.PreviewMouseLeftButtonDown -= TextBox_PreviewMouseLeftButtonDown;
             }
         }
-
-        private static void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        
+        if (d is PasswordBox passwordBox)
         {
-            if (sender is TextBox textBox)
+            if (newValue)
             {
-                textBox.SelectAll();
+                passwordBox.GotFocus += PasswordBox_GotFocus;
+                passwordBox.PreviewMouseLeftButtonDown += PasswordBox_PreviewMouseLeftButtonDown;
+            }
+            else
+            {
+                passwordBox.GotFocus -= PasswordBox_GotFocus;
+                passwordBox.PreviewMouseLeftButtonDown -= PasswordBox_PreviewMouseLeftButtonDown;
             }
         }
+    }
 
-        private static void TextBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    private static void TextBox_GotFocus(object sender, RoutedEventArgs e)
+    {
+        if (sender is TextBox textBox) textBox.SelectAll();
+    }
+
+    private static void TextBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (sender is TextBox textBox && !textBox.IsKeyboardFocusWithin)
         {
-            if (sender is TextBox textBox && !textBox.IsKeyboardFocusWithin)
-            {
-                textBox.Focus();
-                e.Handled = true;
-            }
+            textBox.Focus();
+            e.Handled = true;
+        }
+    }
+
+    private static void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+    {
+        if (sender is PasswordBox passwordBox) passwordBox.SelectAll();
+    }
+
+    private static void PasswordBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (sender is PasswordBox passwordBox && !passwordBox.IsKeyboardFocusWithin)
+        {
+            passwordBox.Focus();
+            e.Handled = true;
         }
     }
 }
